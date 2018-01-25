0e7f1d65c560e0500fe80cba4cf9024c93783ffd
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-01-25 21:58:30 +0200

    Disabled promise manager to make async/await work
    Disabled promise manager to make async/await work

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,5 +3,6 @@ require('ts-node').register();
 module.exports.config = {
     specs: ['spec.ts'],
     directConnect: true,
-    baseUrl: 'https://movies-finder.firebaseapp.com/'
+    baseUrl: 'https://movies-finder.firebaseapp.com/',
+    SELENIUM_PROMISE_MANAGER: false
 }
