8d3726fbdf85ec66fa0c0311b60f843ed2f1da3f
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-01-30 22:00:28 +0200

    Search examples

--- spec.ts
+++ spec.ts
@@ -1,5 +1,30 @@
+import { browser, element, By, $, $$} from 'protractor'
+
 describe('Protractor', function () {
-    it('Should be alive', function () {
-        expect(true).toBe(true)
+    it('Should return elements by css', async function () {
+        await browser.get('/')
+        let a = element.all(By.css('movie-card')).first()
+        let b = $('movie-card') // the same as element(By.css('movie-card'))
+        let c = $$('').last()
+
+        console.log(await a.getText())
+        console.log(await b.getText())     
+        console.log(await c.getText())        
     })
+
+    it('should allow chaining of elements', async function () {
+        await browser.get('/')
+        let a = $$(`movie-card`).get(3).element(By.xpath('..'))
+
+        console.log(await a.getAttribute('href'))
+        // let b = $$(`movie-card h4 a[href*='movie']`).first()
+      
+    })
+
+    it('should allow searching by XPATH', async function () {
+        await browser.get('/')
+        // https://docs.google.com/document/d/11EUbCnzutLiMCGQJzB6ebnKUnOkP7lNNPwJ3cGqBYg0/edit#
+    })
+
+    
 })
