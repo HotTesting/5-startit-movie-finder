e94cee132495de350c2d6c020d65c2cbf6260e78
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-02-07 23:48:16 +0200

    Added home task for 5 lesson
    Added home task for 5 lesson

---
+++ jasminejs/lesson5task.ts
@@ -0,0 +1,45 @@
+import { browser, $, $$, element, by, By } from 'protractor'
+
+describe('Movie details', async function () {
+    it('should have movie name as header', async function () {
+        throw new Error('Test not implemented yet')
+    })
+
+    it('should have raiting', async function () {
+        throw new Error('Test not implemented yet')
+    })
+
+    it('should have simular movies block with atleast one movie', async function () {
+        throw new Error('Test not implemented yet')
+    })
+
+    it('should have simular movies block with atleast one movie', async function () {
+        throw new Error('Test not implemented yet')
+    })
+
+    describe('cast block', async function () {
+        it('should show atleast one actor', async function () {
+            throw new Error('Test not implemented yet')
+        })
+    })
+
+    describe('reviews block', function () {
+        it('should be atleast one review', async function () {
+            throw new Error('Test not implemented yet')
+        })
+
+        it('should have reviewer name as link to source', async function () {
+            throw new Error('Test not implemented yet')
+        })
+    })
+})
+
+describe('Popular series', async function () {
+    it('shouldnt have search bar', async function () {
+        throw new Error('Test not implemented yet')
+    })
+
+    it('should have "First Air Date" instead "Release Date"', async function () {
+        throw new Error('Test not implemented yet')
+    })
+})
